304f6227b8f29892881c91ab145709353bbd43a8
YakovlefS <yakovlef.s@gmail.com>
2022-03-28 20:53:22 +0300

    Initial Commit

---
+++ RGB App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RGB App
+//
+//  Created by YakovlefS on 28.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
